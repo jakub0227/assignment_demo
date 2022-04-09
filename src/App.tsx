69aaa56b01323a7b5009edde29570d76709bcd6c
jakub0227 <jakub.wysocki.1983@gmail.com>
2022-04-09 15:43:05 +0200

    remove not needed files, create hoc folder holding Layout.tsx wrapping component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, { FC } from "react";
+import { Layout } from "./hoc/Layout";
 
 export const App: FC = () => {
   return (
-    <div className="flex flex-col items-center justify-items-start h-screen text-red-300 bg-gradient-to-br from-gray-300 via-teal-700 to-gray-800">
+    <Layout>
       <h1 className="text-3xl mt-10 text-white">Hello Tailwind 👋</h1>
-    </div>
+    </Layout>
   );
 };
---
+++ src/hoc/Layout.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+export const Layout: FC = (props) => {
+  return (
+    <div className="flex flex-col items-center justify-items-start h-screen  bg-gradient-to-br from-gray-300 via-teal-700 to-gray-800">
+      {props.children}
+    </div>
+  );
+};
