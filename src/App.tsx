dddfc4edf9f8a277ce08818ba554d07a68ca1fb8
jakub0227 <jakub.wysocki.1983@gmail.com>
2022-04-09 16:01:21 +0200

    add dynamic route for DetailsPage.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Layout } from "./hoc/Layout/Layout";
+import { DetailsPage } from "./pages/DetailsPage/DetailsPage";
 import { HomePage } from "./pages/HomePage/HomePage";
 
 export const App: FC = () => {
@@ -8,6 +9,7 @@ export const App: FC = () => {
     <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/:id" element={<DetailsPage />} />
       </Routes>
     </Layout>
   );
--- src/pages/DetailsPage/DetailsPage.tsx
+++ src/pages/DetailsPage/DetailsPage.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { FC } from "react";
 
-export const DetailsPage = () => {
-  return <div>DetailsPage</div>;
+export const DetailsPage: FC = () => {
+  return (
+    <div>
+      <h1 className="text-4xl mt-10 text-white">Hello from details of ID:</h1>
+    </div>
+  );
 };
